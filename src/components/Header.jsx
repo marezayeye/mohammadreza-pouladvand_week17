10bb2b4482914bd3158ff40c50e53da3e34c50b8
marezayeye <marezayeye@gmail.com>
2025-08-05 13:55:00 +0330

    week17: miron Header Fixes, Added new contacts to JSON

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <div className={styles.container}>
-      <h1>Contact App (Flux Architecture)</h1>
+      <h1>Contact App v3.0</h1>
       <p>
         <a href="https://github.com/marezayeye">Github Profile</a> | Botostart
-        Bootcamp | Week 16
+        Bootcamp | Week 17
       </p>
     </div>
   );
